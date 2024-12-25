1844bc0dbf46413ded315cab4f9ddaf6c8098735
Vladislav Chudaev <vladislav.chudaev1998>
2024-12-25 18:04:00 +0300

    Add project files.

---
+++ SimpleClassExample/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        public string carName;
+        public int currentSpeed;
+
+        public Car()
+        {
+            carName = "Car";
+            currentSpeed = 0;
+        }
+
+        public Car(string cn)
+        {
+            carName = cn;
+        }
+
+        public Car(string cn, int cs)
+        {
+            carName = cn;
+            currentSpeed = cs;
+        }
+
+
+        public void PrintState() => Console.WriteLine($"{carName} is going {currentSpeed} MPH");
+
+        public void SpeedUp(int delta) => currentSpeed += delta;
+    }
+}
---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SimpleClassExample;
+
+
+Console.WriteLine("=> Fun with Class Types.\n");
+
+
+Car defaultCar = new Car("Mitsubisi");
+defaultCar.PrintState();
+Console.ReadLine();
+
+Car myCar = new Car();
+myCar.carName = "Nissan Skyline";
+myCar.currentSpeed = 10;
+
+for (int i = 0; i < 10; i++)
+{
+    myCar.PrintState();
+    myCar.SpeedUp(5);
+}
