27038072ec2814517cdb0d19615365502d6a58c2
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-01-14 22:17:03 +0300

    statics

---
+++ StaticDataAndMembers/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using StaticDataAndMembers;
+
+
+Console.WriteLine("***** Fun with static data *****\n");
+
+SavingsAccount s1 = new SavingsAccount(50);
+SavingsAccount s2 = new SavingsAccount(100);
+SavingsAccount s3 = new SavingsAccount(10000.75);
+Console.ReadLine();
