15748a80c94bdc904aabc7001d8d5d7486aaa853
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-01-26 22:38:39 +0300

    properties

--- EmployeeApp/Employee.cs
+++ EmployeeApp/Employee.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace EmployeeApp
 {
@@ -8,7 +9,52 @@ namespace EmployeeApp
         private string _empName;
         private int _empId;
         private float _currPay;
+        private int _empAge;
 
+        //properties
+        public string Name
+        {
+            get
+            { 
+                if (_empName == null)
+                {
+                    return "There is no values! null!";
+                }
+                else
+                {
+                    return _empName;
+                }
+            }
+            set
+            {
+                if (value.Length > 15)
+                {
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                }
+                else
+                {
+                    _empName = value;
+                }
+            }
+        }
+
+        public int Id
+        {
+            get { return _empId; }
+            set { _empId = value; }
+        }
+        public float CurrentPay
+        {
+            get { return _currPay; }
+            set { _currPay = value; }
+        }
+        public int Age
+        {
+            get { return _empAge; }
+            set { _empAge = value; }
+        }
+
+        /*
         //getter
         public string GetName () => _empName;
 
@@ -24,23 +70,27 @@ namespace EmployeeApp
                 _empName = name;
             }
         }
+        */
 
         //ctors
         public Employee() { }
-        public Employee(string name, int id, float pay)
+        public Employee(string name, int id, float pay) :this (name, id, pay, 0) {}
+        public Employee(string name, int id, float pay, int age)
         {
             _empName = name;
             _empId = id;
             _currPay = pay;
+            _empAge = age;
         }
 
         //methods
-        public void GiveBonus(float amount) => _currPay = amount;
+        public void GiveBonus(float amount) => _currPay += amount;
         public void DisplayStatus()
         {
             Console.WriteLine($"Name: {_empName}");
             Console.WriteLine($"ID: {_empId}");
             Console.WriteLine($"Pay: {_currPay}");
+            Console.WriteLine($"Age: {_empAge}");
         }
     }
 }
--- EmployeeApp/Program.cs
+++ EmployeeApp/Program.cs
@@ -3,7 +3,24 @@ using EmployeeApp;
 
 
 
-Employee emp = new Employee();
-emp.SetName("Vladik");
+Console.WriteLine("***** Fun with Encapsulation *****\n");
+
+Employee emp = new Employee("Marvin", 456, 30_000);
+//emp.Name = "stacy";
+//emp.SetName("Vladik");
 emp.GiveBonus(100_000_000);
 emp.DisplayStatus();
+
+Console.ReadLine();
+
+
+Employee emp2 = new Employee();
+//emp2.SetName("Xena the warrior princess");
+
+Console.WriteLine(emp2.Name);
+
+Console.ReadLine();
+
+
+Employee joy = new Employee();
+joy.Age++;
