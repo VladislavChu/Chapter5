0565e6438226b0b998f16a5bad8bc680d5db08ca
Константин Ладанин <vladislav.chudaev1998@gmail.com>
2025-02-19 09:08:59 +0300

    Completed the chapter 5

--- FunWithRecords/Program.cs
+++ FunWithRecords/Program.cs
@@ -20,6 +20,10 @@ Console.WriteLine();
 
 Console.ReadLine();
 
+Console.WriteLine($"Car are the same? {myCar.Equals(anotherCar)}");
+Console.WriteLine($"Car are the same reference? {ReferenceEquals (myCar, anotherCar)}");
+
+
 Console.WriteLine("/********************RECORDS********************/");
 
 //CarRecord myCarRecord = new CarRecord
@@ -37,6 +41,24 @@ Console.WriteLine("Another variable for my car: ");
 Console.WriteLine(anotherMyCarRecord.ToString());
 Console.WriteLine();
 
+Console.WriteLine($"CarRecords are the same? {myCarRecord.Equals(anotherMyCarRecord)}");
+Console.WriteLine($"CarRecords are the same? {ReferenceEquals (myCarRecord, anotherMyCarRecord)}");
+Console.WriteLine($"CarRecords are the same? {myCarRecord == anotherMyCarRecord}");
+Console.WriteLine($"CarRecords are not the same? {myCarRecord != anotherMyCarRecord}");
+Console.ReadLine();
+
+CarRecord carRecordCopy = anotherMyCarRecord;
+Console.WriteLine($"CarRecordCopy are the same? {carRecordCopy.Equals(anotherMyCarRecord)}");
+Console.WriteLine($"CarRecordCopy are the same? {ReferenceEquals(carRecordCopy, anotherMyCarRecord)}");
+Console.ReadLine();
+
+CarRecord outOtherCar = myCarRecord with { Model = "Odyssey" };
+Console.WriteLine("My copied car: ");
+Console.WriteLine(outOtherCar.ToString());
+Console.WriteLine("Car Record copy using with expression results");
+Console.WriteLine($"CarRecordCopy are the same? {outOtherCar.Equals(myCarRecord)}");
+Console.WriteLine($"CarRecordCopy are the same? {ReferenceEquals(outOtherCar, myCarRecord)}");
+Console.ReadLine();
 
 
 static void DisplayCarStats(Car c)
